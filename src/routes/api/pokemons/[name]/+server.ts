53c34a1bc9d51c00eaa4fb994f1f33caa14433c3
SchlammSpringer <schlammspringer@mailbox.org>
2023-04-08 16:19:44 +0200

    🏗️ api caches

--- src/routes/api/pokemons/[name]/+server.ts
+++ src/routes/api/pokemons/[name]/+server.ts
@@ -1,22 +1,28 @@
 import { findPokemonByName, updatePokemon } from '$lib/server/db-queries'
 import { validatePokemon } from '$lib/server/share'
 import type { Pokemon } from '$lib/types'
-import type { RequestEvent, RequestHandler } from '@sveltejs/kit'
+import type { RequestHandler } from '@sveltejs/kit'
 import { error, error as svelteError, json } from '@sveltejs/kit'
 
 export const GET = (async ({ params }) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const { data } = await findPokemonByName(params.name!)
   if (!data || data?.length === 0) throw error(404, 'not found')
-  return json(data[0].pokemon)
+  return json(data[0].pokemon, {
+    headers: { 'Cache-Control': 's-maxage=1, stale-while-revalidate' }
+  })
 }) satisfies RequestHandler
 
-export const PUT = async (event: RequestEvent) => {
-  const body: Pokemon = (await event.request.json()) as Pokemon
+export const PUT = async ({ request }) => {
+  console.log('put', request)
+
+  const body: Pokemon = (await request.json()) as Pokemon
   const form = await validatePokemon(body)
   const { data } = await updatePokemon(body)
   if (!data) {
     throw svelteError(400, `validation errors: ${JSON.stringify(form.errors, null, 2)}`)
   }
-  return json(data[0].pokemon)
+  return json(data[0].pokemon, {
+    headers: { 'Cache-Control': 's-maxage=1, stale-while-revalidate' }
+  })
 }
--- src/routes/pokemons/[name]/+page.server.ts
+++ src/routes/pokemons/[name]/+page.server.ts
@@ -1,11 +1,11 @@
-import { BYPASS_TOKEN, VERCEL_URL } from '$env/static/private'
+import { BYPASS_TOKEN, VERCEL_URL, VITE_VERCEL_ENV } from '$env/static/private'
 import { validatePokemon } from '$lib/server/share'
 import { schema, type Pokemon } from '$lib/types'
 import { fail, error as svelteError } from '@sveltejs/kit'
 import { superValidate } from 'sveltekit-superforms/server'
 import type { Actions, PageServerLoad } from './$types'
 
-export const load = (async ({fetch, params}) => {
+export const load = (async ({ fetch, params }) => {
   const response: Response = await fetch(`/api/pokemons/${params.name}`)
   if (response.status !== 200) {
     throw svelteError(response.status, `${params.name} not found`)
@@ -16,7 +16,7 @@ export const load = (async ({fetch, params}) => {
 }) satisfies PageServerLoad
 
 export const actions = {
-  default: async ({fetch, request, params}) => {
+  default: async ({ fetch, request, params }) => {
     const form = await superValidate(request, schema)
 
     // Convenient validation check:
@@ -28,15 +28,15 @@ export const actions = {
       return fail(400, { form })
     }
 
-    console.log(BYPASS_TOKEN, VERCEL_URL)
-    // TODO: Do something with the validated data
-    const response = await fetch(`https://${VERCEL_URL}/pokemons/${params.name}`, {
-      headers: { 'x-prerender-revalidate': BYPASS_TOKEN },
-      method: 'HEAD'   
-    })
+    console.log(BYPASS_TOKEN, VERCEL_URL, VITE_VERCEL_ENV)
+    if (VITE_VERCEL_ENV !== 'development') {
+      const response = await fetch(`https://${VERCEL_URL}/pokemons/${params.name}`, {
+        headers: { 'x-prerender-revalidate': BYPASS_TOKEN },
+        method: 'HEAD'
+      })
 
-    console.log('response from resfresh: ' ,response.status)
-
+      console.log('response from resfresh: ', response.status)
+    }
     await fetch(`/api/pokemons/${params.name}`, {
       method: 'PUT',
       body: JSON.stringify(form.data)
