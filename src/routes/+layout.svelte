c73c406edf218d40b0db76c44514b8b81c192ca0
SchlammSpringer <schlammspringer@mailbox.org>
2023-04-09 23:09:50 +0200

    ➖ font awesome

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,7 +50,10 @@
         <div class="flex items-center">
           <button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
             <span>
-              <i class="text-2xl fa-solid fa-bars" />
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                   stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
             </span>
           </button>
           <strong class="text-lg md:text-xl">Pokedex</strong>
