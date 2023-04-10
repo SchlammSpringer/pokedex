b29a3a5215f0100181abb3229b4bf2d568c286d0
SchlammSpringer <schlammspringer@mailbox.org>
2023-04-10 07:43:50 +0200

    🚧 🧪  api

--- src/routes/api/pokemons/[name]/+server.ts
+++ src/routes/api/pokemons/[name]/+server.ts
@@ -2,12 +2,12 @@ import { findPokemonByName, updatePokemon } from '$lib/server/db-queries'
 import { validatePokemon } from '$lib/server/share'
 import type { Pokemon } from '$lib/types'
 import type { RequestHandler } from '@sveltejs/kit'
-import { error, error as svelteError, json } from '@sveltejs/kit'
+import { error as svelteError, json } from '@sveltejs/kit'
 
 export const GET = (async ({ params }) => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const { data } = await findPokemonByName(params.name!)
-  if (!data || data?.length === 0) throw error(404, 'not found')
+  if (!data || data?.length === 0) throw svelteError(404, 'not found')
 
   return json(data[0].pokemon, {
     headers: { 'Cache-Control': 's-maxage=1, stale-while-revalidate' }
--- src/routes/api/pokemons/[name]/server.test.ts
+++ src/routes/api/pokemons/[name]/server.test.ts
@@ -1,18 +1,85 @@
-import { describe, it } from 'vitest'
+import type { Mock } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { GET } from './+server'
+import { findPokemonByName } from '$lib/server/db-queries'
+import type { RequestEvent } from '@sveltejs/kit'
+import { error, json } from '@sveltejs/kit'
+import { mock } from 'vitest-mock-extended'
+import type { Pokemon } from '$lib/types'
+
+const convertError = () => (statuscode: number, message: string) =>
+  new Error(`${statuscode}: ${message}`)
+
+interface Response {
+  pokemon: Pokemon
+  header: Record<string, string>
+}
+
+const convertResponse = () => (pokemon: Pokemon, header: Record<string, string>) =>
+  ({
+    pokemon,
+    header
+  } satisfies Response)
 
 describe('single Pokemon API', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  vi.mock('@sveltejs/kit')
+  vi.mock('$lib/server/db-queries')
+  const request = mock<RequestEvent>()
+
+  const pokemon = {
+    pokedex: 1,
+    name: 'bulbasaur',
+    description: 'Dieses Pokémon',
+    types: ['grass', 'poison'],
+    color: 'green',
+    germanName: 'Bisasam',
+    habitat: 'grassland'
+  }
+
   describe('GET', () => {
-    it('sucess', async () => {})
-    it('error', async () => {
-      const response = await GET({
-        params: {
-          name: 'charmander'
-        }
+    it('sucess', async () => {
+      request.params = {
+        name: 'charmander'
+      }
+      const header = {
+        headers: { 'Cache-Control': 's-maxage=1, stale-while-revalidate' }
+      }
+      const mockedfindPokemonByName = findPokemonByName as Mock
+      const mockedJson = json as Mock
+      mockedJson.mockImplementation(convertResponse())
+      mockedfindPokemonByName.mockResolvedValue({
+        data: [
+          {
+            pokemon: pokemon
+          }
+        ]
       })
-      console.log(response)
+
+      const response = await GET(request)
+
+      expect(mockedfindPokemonByName).toHaveBeenNthCalledWith(1, 'charmander')
+      expect(mockedJson).toHaveBeenNthCalledWith(1, pokemon, header)
+      expect(response).toStrictEqual({ pokemon, header })
+    })
+    it('error', async () => {
+      request.params = {
+        name: 'notExist'
+      }
+      const mockedfindPokemonByName = findPokemonByName as Mock
+      const mockedError = error as Mock
+      mockedError.mockImplementation(convertError())
+      mockedfindPokemonByName.mockResolvedValue({
+        data: []
+      })
+
+      await expect(GET(request)).rejects.toThrowError('404: not found')
     })
   })
+
   describe('PUT', () => {
     it('sucess', async () => {
       // PUT({'name': 'Shitty-Charmander'} )
