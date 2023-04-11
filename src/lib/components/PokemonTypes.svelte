2631c53124909074c402b42a39a294bb973df275
SchlammSpringer <schlammspringer@mailbox.org>
2023-04-11 08:19:41 +0200

    ⬆️

--- src/lib/components/PokemonTypes.svelte
+++ src/lib/components/PokemonTypes.svelte
@@ -9,7 +9,7 @@
 {:else}
   <h3>Types</h3>
 {/if}
-<span>Choose a specific type, to show all Pokemons of type</span>
+<span>Choose a specific type, to show classic Pokemons of type</span>
 <div class="flex justify-start items-center space-x-4">
   {#each pokemon.types as type}
     <a class="chip variant-soft hover:variant-filled !no-underline" href="/pokemons?type={type}">
