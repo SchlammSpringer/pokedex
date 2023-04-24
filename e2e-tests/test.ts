97ca258330369484d4cbfe7aa89eea183ce650a5
SchlammSpringer <schlammspringer@mailbox.org>
2023-04-24 16:38:21 +0200

    🚀 try checkly

--- e2e-tests/test.ts
+++ e2e-tests/test.ts
@@ -2,16 +2,16 @@ import { expect, test } from '@playwright/test'
 
 test('workflow', async ({ page }) => {
   await page.goto('/')
-  
+
   await expect(
     page.getByRole('heading', { name: 'Educational poject with SvelteKit & PokeAPI' })
   ).toBeVisible()
-  
+
   const navigation = page.getByRole('navigation')
   expect(navigation).toBeVisible()
-  
+
   navigation.getByRole('link', { name: 'Pokemons' }).click()
-  
+
   await expect(page).toHaveURL(/\/pokemons/)
   await expect(page.getByRole('heading', { name: 'Pokemons' })).toBeVisible()
 
@@ -19,7 +19,13 @@ test('workflow', async ({ page }) => {
 
   await page.getByRole('link', { name: 'show details for pikachu' }).click()
 
-  await expect(page).toHaveURL(/\/pokemons/)
+  await expect(page).toHaveURL(/\/pokemons\/pikachu/)
 
+  await expect(page.getByRole('heading', { level: 1, name: 'pikachu' })).toBeVisible()
 
+  // await expect(page.getByRole('heading', { level: 2, name: 'Pikachu' })).toBeVisible()
+
+  await page.getByRole('link', { name: 'electric' }).click()
+
+  await expect(page).toHaveURL(/\/pokemons\?type=electric/)
 })
